97286687019a91a97fe9c12896ad17727d4c524b
chris <chris@caat.org.uk>
2020-10-14 18:52:34 +0100

    unknown TR. test_page_links.py now contains code which generates some 550 discrete URL tests... this is just for the home page and top banner 4 pages. Due to site defences and not recognising my IP as friendly a lot of these are 503, but many are real bad links

--- page_links/test_page_links.py
+++ page_links/test_page_links.py
@@ -2,17 +2,28 @@ import requests
 import pytest
 from bs4 import BeautifulSoup
 
-def get_all_links_from_home_page():
-    url = 'http://www.caat.org.uk/' 
-    response = requests.get(url)
-    soup = BeautifulSoup( response.text, 'html.parser' )
-    return list( soup.html.body.select( 'a' ))
+def get_all_links_from_pages():
+    # NB this now produces some 550 links from the home page + 4 "top banner" pages - 
+    # the test took 3 1/2 minutes the first time it was run... but will normally 
+    # probably take longer
+
+    page_names =  [ '', 'about-caat', 'news', 'events', 'resources', ]
+    a_links = []
+    for page_name in page_names:
+        url = 'http://www.caat.org.uk/' + page_name
+        response = requests.get(url)
+        soup = BeautifulSoup( response.text, 'html.parser' )
+        a_links.extend( [ ( x, page_name ) for x in soup.html.body.select( 'a' )] )
+    return a_links
 
 def pytest_generate_tests(metafunc):
-    metafunc.parametrize( 'a_link', get_all_links_from_home_page() )
+    metafunc.parametrize( 'link_tuple', get_all_links_from_pages() )
 
-def test_all_links_200(a_link):
-    assert a_link.has_attr( 'href' ), f'link without href: {a_link}'
+def test_all_links_200(link_tuple):
+    a_link = link_tuple[ 0 ]
+    page_name = link_tuple[ 1 ]
+
+    assert a_link.has_attr( 'href' ), f'page /{page_name}: link without href: {a_link}'
     url = a_link[ 'href' ]
     # NB found with various links e.g. in home page... raises Exception in requests.get
     if url == '/' or url == '#':
@@ -21,21 +32,8 @@ def test_all_links_200(a_link):
         response = requests.get( url )
     except Exception as e:
         print( 'requests.get() raised Exception: ', e )
-        err_msg = f'a-link |{a_link}| with URL |{url}| rejected as invalid: examine URL'
+        err_msg = f'page /{page_name}: a-link |{a_link}| with URL |{url}| rejected as invalid: examine URL'
         print( err_msg )
         raise Exception( err_msg )
-    err_msg = f'on page home page: http request to page {url} returned incorrect status code: {response.status_code}. Expected: 200'
+    err_msg = f'page /{page_name}: http request to page {url} returned incorrect status code: {response.status_code}. Expected: 200'
     assert response.status_code == 200, err_msg
-
-
-
-# # @pytest.mark.skip
-# @pytest.mark.parametrize('page_name', [ 'about-caat', 'news', 'events', 'resources', ])         
-# def test_top_of_page_banner_pages_a_links( page_name ):
-#     url = 'http://www.caat.org.uk/' + page_name
-#     response = requests.get(url)
-#     global soup 
-#     soup = BeautifulSoup( response.text, 'html.parser' )
-#     check_page_links( soup.html.body, f'home/{page_name}')
-
-
