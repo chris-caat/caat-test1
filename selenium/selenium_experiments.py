4c06b960bfa987fdf77c3797848dbed7006b874d
chris <chris@caat.org.uk>
2020-10-20 13:04:53 +0100

    Selenium using FF headless mode

--- selenium/selenium_experiments.py
+++ selenium/selenium_experiments.py
@@ -12,24 +12,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 import time
 
-driver = webdriver.Firefox( executable_path=GeckoDriverManager().install() )
+from selenium.webdriver.firefox.options import Options
 
+ff_options = Options()
+ff_options.headless = True
 
-# driver = webdriver.Firefox()
-driver.get("http://www.caat.org.uk") # put here the adress of your page
+driver = webdriver.Firefox( options=ff_options, executable_path=GeckoDriverManager().install() )
+
+driver.get("http://www.caat.org.uk") 
 elem = driver.find_elements_by_xpath( "//*[@type='submit']" )
-# put here the content you have put in Notepad, ie the XPath
-# button = driver.find_element_by_id('buttonID') # Or find button by ID.
-# print(elem.get_attribute("class"))
 
-
-
-# driver.implicitly_wait(10)
-
-
+# click accept cookies button when present
 timeout = 5
 try:
-    # accept cookies button:
     element_present = EC.presence_of_element_located(( By.ID, 'ccc-recommended-settings' ))
     WebDriverWait(driver, timeout).until(element_present)
 except TimeoutException:
@@ -41,22 +36,14 @@ while True:
     time.sleep( 1 )
     print( "blip")
 rec_settings_button.click()
-# go to Alternatives page
-alternatives_url = 'https://caat.org.uk/alternatives/'
-# driver.find_element_by_xpath('//a[@href="'+ alternatives_url +'"]').click()  
 
 # go to Companies page
-# <a href="https://caat.org.uk/resources/companies/">Arms companies</a>
 companies_url = 'https://caat.org.uk/resources/companies/'
-
 driver.find_element_by_xpath('//a[@href="'+ companies_url +'"]').click()  
 
-
-# <input id="dh-input-nameSearch-8156859" class="dh-filter-input dh-input-nameSearch" type="text" size="1" placeholder="Company name">
-
+# go to name INPUT
 timeout = 15
 try:
-
     element_present = EC.presence_of_element_located((By.CLASS_NAME, 'dh-input-nameSearch'))
     WebDriverWait(driver, timeout).until(element_present)
 except TimeoutException:
@@ -70,12 +57,12 @@ while True:
     print( "blip")
 
 
-
-
-# <input id="dh-input-nameSearch-8156859" class="dh-filter-input dh-input-nameSearch" type="text" size="1" placeholder="Company name">
+# search on "Boeing"
 name_input.send_keys( 'Boeing\n' )
 
 
-
 time.sleep( 5 )
+
+
+print( '... closing')
 driver.close()
