4c0081ce9670de3025c9176ae200afb287baa30e
chris <chris@caat.org.uk>
2021-04-11 19:35:19 +0100

    DEPLOYED v0.0.1. Versioning file added

--- url_test_authentication/test_caat_url_authent.py
+++ url_test_authentication/test_caat_url_authent.py
@@ -5,14 +5,27 @@ import os
 print( "here" )
 print( os.getcwd() )
 
-file = open( '../../data.txt' )
+
+# NB this directory is called "third_post" to make it look uninteresting
+# D:\My Documents\sysadmin\Resources\third_post\data.txt
+creds_path = os.path.expandvars(r'$SYSADMIN\Resources\third_post\data.txt')
+print(f'creds_path |{creds_path}|')
+file = open(creds_path)
+
+
 user_line = file.readline()
 user = user_line.split( ':' )[ 1 ].strip()
 pwd_line = file.readline()
 password = pwd_line.split( ':' )[ 1 ].strip()
 
+print( f'user_line |{user_line}|, pwd_line |{pwd_line}|')
+
 def test_example():
+    print('running test...')
     url = "https://2020.caat.org.uk/"
     r = requests.get( url, auth=HTTPBasicAuth(user, password))
     assert 200 == r.status_code
+    
+
+test_example()
     
